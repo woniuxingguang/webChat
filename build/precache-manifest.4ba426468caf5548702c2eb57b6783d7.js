5abcb0a8e8d79db7305941e5cb7346f56bfa7014
woniuxingguang <woniuxingguang@126.com>
2018-11-20 09:26:30 +0800

    build

---
+++ build/precache-manifest.4ba426468caf5548702c2eb57b6783d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb284521ef729eb637dcdb075a7d8887",
+    "url": "./static/media/timg.fb284521.jpg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "0e26136da6c283adf944",
+    "url": "./static/js/main.0e26136d.chunk.js"
+  },
+  {
+    "revision": "713fe9c5ad226722660f",
+    "url": "./static/js/1.713fe9c5.chunk.js"
+  },
+  {
+    "revision": "0aacc66eefd0452fb4aa7fa20e2d3b08",
+    "url": "./index.html"
+  }
+];
